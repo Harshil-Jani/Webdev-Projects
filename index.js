c18c10f6eb41ea3fd1717f1d797a9fef5f54a736
Harshil Jani <harshiljani2002@gmail.com>
2022-02-04 15:40:03 +0530

    Added New React Project and Updated Main UI more.

---
+++ index.js
@@ -0,0 +1,4 @@
+const copyright = document.getElementById("copyright");
+const year = new Date().getFullYear();
+
+copyright.innerText = `© Copyrights ${year} Web-Devlopment Projects by Harshil Jani`;
